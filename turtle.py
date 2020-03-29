1f4aac42aa911d21188051d3c57bff34d17de785
AkilaBandara <59506472+AkilaBandara-git@users.noreply.github.com>
2020-03-29 15:30:43 +0530

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,31 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+def draw():
+	my_turtle.forward(100)
+	my_turtle.right(90)
+	my_turtle.forward(100)
+	my_turtle.right(90)
+	my_turtle.forward(100)
+	my_turtle.right(90)
+	my_turtle.forward(100)
+
+t = True
+while t:
+	draw()
+	my_turtle.forward(150)
+	draw()
+	my_turtle.forward(150)
+	draw()
+
+# this is python inbuild module and don't need pip install
+# this code is inmy github turtle repo
+# Folow me:www.twitter.com/@akilaontweet
+
+
+
+
+
+
+#####################BYE################
